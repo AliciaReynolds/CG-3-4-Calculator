ad59022cab5d99fb1f6a6780a12d179403c52888
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-25 21:24:23 -0500

    Add variables

--- CG 3-4 Calculator/Program.cs
+++ CG 3-4 Calculator/Program.cs
@@ -6,7 +6,26 @@ namespace CG_3_4_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Coming up with opening sentences have never been my
+            //strong suit.  No matter what I have to write.
+
+            Console.WriteLine("Welcome to Alici'a crazy adding machine.");
+            Console.WriteLine("You pick the numbers and the operation and we'll give you an answer.");
+            Console.WriteLine("First, please provide any number between 1 and a million.");
+
+            var No1 = Console.ReadLine();
+
+            Console.WriteLine("Thank you! Now please provid a second number, again any number between 1 and a million.");
+
+            var No2 = Console.ReadLine();
+
+            var answer = No1 + No2;
+
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
         }
     }
 }

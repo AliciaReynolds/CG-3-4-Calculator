ca8e519deaf16bdaa35236ec4805e4700dc4e8ff
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-25 22:10:42 -0500

    Fixed my input problem to get operations fuctioning

--- CG 3-4 Calculator/Program.cs
+++ CG 3-4 Calculator/Program.cs
@@ -7,26 +7,28 @@ namespace CG_3_4_Calculator
         static void Main(string[] args)
         {
 
-            //Coming up with opening sentences have never been my
-            //strong suit.  No matter what I have to write.
-            //Hope you don't mind a little fun
+            /*Coming up with opening sentences have never been my
+            strong suit.  No matter what I have to write.
+            Hope you don't mind a little fun*/
 
             Console.WriteLine("Welcome to Alici'a crazy adding machine.");
             Console.WriteLine("You pick the numbers and the operation and we'll give you an answer.");
             Console.WriteLine("First, please provide any number between 1 and a million.");
 
-            //This sets up the app to accept the first inputted number by
-            //setting the variable to be given by whomever is inputting and
-            //for the code to include that into the equation.
+            /*This sets up the app to accept the first inputted number by
+            setting the variable to be given by whomever is inputting and
+            for the code to include that into the equation.*/
 
             var No1 = Console.ReadLine();
+            int input1 = Convert.ToInt32(No1);
 
             Console.WriteLine("Thank you! Now please provid a second number, again any number between 1 and a million.");
 
-            //This is just like above, with setting the variable to be an inputted 
-            //number to be used later on in the coding for the calulator part
+            /*This is just like above, with setting the variable to be an inputted 
+            number to be used later on in the coding for the calulator part*/
 
             var No2 = Console.ReadLine();
+            int input2 = Convert.ToInt32(No2);
 
             Console.WriteLine("Thank you again for that. Now please choose the operation you would like to use.");
             Console.WriteLine("You may choose one of the following: add, subtract, multiply, or divide.");
@@ -35,8 +37,7 @@ namespace CG_3_4_Calculator
             
             if (operation == "add")
             {
-                var add = Convert.ToInt32(add);
-                var answeradd = (No1) + (No2);
+               var answeradd = input1 + input2;
                 Console.WriteLine(answeradd);
             }
 
